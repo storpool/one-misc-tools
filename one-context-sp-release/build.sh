c9edd36d51bea779529b29bb8cded38be27436c4
Anton Todorov <a.todorov@storpool.com>
2019-08-23 13:08:02 +0300

    initial add of one-context-sp-release

---
+++ one-context-sp-release/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REPOURL=${REPOURL:-http://repos.storpool.com/public/one-context-sp/centos/\$releasever/noarch}
+
+if ! which rpmbuild >/dev/null 2>&1; then
+	echo "Error rpmbuild not found! Install with " >&2
+	echo " yum install -y rpm-build" >&2
+	exit 1
+fi
+
+mkdir -p ~/rpmbuild/{SOURCES,SPECS,SRPMS,RPMS/noarch}
+
+cp ./CentOS-one-context-sp.repo ./LICENSE ~/rpmbuild/SOURCES/
+cp one-context-sp-release.spec ~/rpmbuild/SPECS
+
+sed -i "s|__REPOURL__|$REPOURL|g" ~/rpmbuild/SOURCES/CentOS-one-context-sp.repo 
+
+rpmbuild -ba ~/rpmbuild/SPECS/one-context-sp-release.spec
+
